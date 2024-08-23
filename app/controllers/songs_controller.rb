31c19659d90cc2a1aaf1309a82d2854aa80d745b
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-22 20:17:55 -0400

    Add instance variable to call in helper

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,8 +43,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    if params{:artist_id}
+    if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
+      @nested = params[:artist_id].present?
       if artist.nil?
         redirect_to artists_path, alert: "Artist not found."
       else
@@ -53,6 +54,7 @@ class SongsController < ApplicationController
       end
     else
       @song = Song.find(params[:id])
+      @nested = false
     end
   end
 
